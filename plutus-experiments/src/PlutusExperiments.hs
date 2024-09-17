80368e4337852ca79b06e48eaffa43add33aebee
paluh (aka Tomasz Rybarczyk) <paluho@gmail.com>
2024-09-17 11:02:26 +0200

    Reconfirm that the final plutus bytecode includes invalid bytes

--- plutus-experiments/src/PlutusExperiments.hs
+++ plutus-experiments/src/PlutusExperiments.hs
@@ -1,24 +1,19 @@
+{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
 {-# OPTIONS_GHC -ddump-splices #-}
 {-# OPTIONS_GHC -ddump-to-file #-}
+{-# OPTIONS_GHC -fno-omit-interface-pragmas #-}
+{-# OPTIONS_GHC -fno-specialise #-}
 
 module PlutusExperiments where
 
-import PlutusExperiments.Hex (_0x)
 import PlutusTx (
     CompiledCode,
     compile,
-    liftCodeDef,
-    unsafeApplyCode,
  )
 import PlutusTx.Prelude (BuiltinByteString)
 
-test :: BuiltinByteString
-test = [_0x|ff|]
-
-plutusFn :: () -> BuiltinByteString
-plutusFn = const test
-
-compiled :: () -> CompiledCode BuiltinByteString
-compiled = unsafeApplyCode $$(compile [||plutusFn||]) . liftCodeDef
+builtinByteStringConstant :: CompiledCode BuiltinByteString
+builtinByteStringConstant = $$(compile [||"\xff"||])
--- plutus-experiments/test/Spec.hs
+++ plutus-experiments/test/Spec.hs
@@ -5,23 +5,47 @@
 
 module Main where
 
+import Control.Monad (when)
 import qualified Data.ByteString as BS
+import Data.ByteString.Base16 as Base16
+import Data.ByteString.Short (fromShort)
+import qualified Data.ByteString.Short as Short
+import qualified Data.List as L
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
+import PlutusExperiments (builtinByteStringConstant)
 import PlutusExperiments.Hex (_0x)
+import PlutusLedgerApi.V3 (serialiseCompiledCode)
 import PlutusTx.Builtins.Internal (BuiltinByteString (BuiltinByteString))
 import Test.Hspec
 
 main :: IO ()
 main = hspec $ do
+    describe "Compiled code contains desired byte" $ do
+        let bytes = serialiseCompiledCode builtinByteStringConstant
+        it "contains the byte" $ do
+            when
+                (0xFF `L.notElem` Short.unpack bytes && [0xC3, 0xBF] `L.isInfixOf` Short.unpack bytes)
+                ( fail $
+                    "Byte not found in compiled code:" <> T.unpack (T.decodeUtf8 (Base16.encode (fromShort bytes))) ::
+                    IO ()
+                )
+
     describe "ByteString Literal Limitations" $ do
-        it "BuiltinByteString handles bytes correctly through character repr" $ do
+        it "BuiltinByteString handles `xFF` bytes correctly through character repr" $ do
             let bb = "ÿ" :: BuiltinByteString
                 BuiltinByteString bs = bb
             bs `shouldBe` BS.pack [0xFF]
 
-        it "BuiltinByteString handles bytes correctly through escape sequence" $ do
+        it "BuiltinByteString handles `xFF` bytescorrectly through escape sequence" $ do
             let bb = "\xff" :: BuiltinByteString
                 BuiltinByteString bs = bb
             bs `shouldBe` BS.pack [0xFF]
+
+        it "BuiltinByteString does not encode `xFF` using UTF-8" $ do
+            let bb = "\xff" :: BuiltinByteString
+                BuiltinByteString bs = bb
+            bs `shouldNotBe` BS.pack [0xC3, 0xBF]
 
         it "ByteString handles bytes correctly" $ do
             let bs = "ÿ" :: BS.ByteString
